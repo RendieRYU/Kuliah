5787e6f0b7fed0277db0e5b51022d2ad75c12f67
Rendie Abdi Saputra <2200018094@webmail.uad.ac.id>
2023-05-30 21:41:14 +0700

    Add files via upload

--- Semester 2/Matematika Diskret/Pertemuan 6/Post6Matdis.cpp
+++ Semester 2/Matematika Diskret/Pertemuan 6/Post6Matdis.cpp
@@ -19,28 +19,25 @@ class count{
 void count::watermark(){
     wm = 5;
     for (int i = 0; i < 2; i++){
-        cout << setw(80);
         for(int j = 0; j < 21; j++){
             cout << wm << " ";
         }
         cout << endl;
     }
-    cout << setw(80) << wm << " " << wm << "      Program Kalkulator FPB       " << wm << " " << wm << endl;
+    cout << wm << " " << wm << "      Program Kalkulator FPB       " << wm << " " << wm << endl;
     for (int i = 0; i < 2; i++){
-        cout << setw(80);
         for(int j = 0; j < 21; j++){
             cout << wm << " ";
         }
         cout << endl;
     }
-    cout << setw(80) << wm << " " << wm << "  Nama      : Rendie Abdi Saputra  " << wm << " " << wm << endl;
-    cout << setw(80) << wm << " " << wm << "  NIM       : 2200018094           " << wm << " " << wm << endl;
-    cout << setw(80) << wm << " " << wm << "  Kelas     : A                    " << wm << " " << wm << endl;
-    cout << setw(80) << wm << " " << wm << "  Praktikum : Matematika Diskrit   " << wm << " " << wm << endl;
-    cout << setw(80) << wm << " " << wm << "  Slot      : Selasa 10.30         " << wm << " " << wm << endl;
-    cout << setw(80) << wm << " " << wm << "  Pertemuan : 6                    " << wm << " " << wm << endl;
+    cout << wm << " " << wm << "  Nama      : Rendie Abdi Saputra  " << wm << " " << wm << endl;
+    cout << wm << " " << wm << "  NIM       : 2200018094           " << wm << " " << wm << endl;
+    cout << wm << " " << wm << "  Kelas     : A                    " << wm << " " << wm << endl;
+    cout << wm << " " << wm << "  Praktikum : Matematika Diskrit   " << wm << " " << wm << endl;
+    cout << wm << " " << wm << "  Slot      : Selasa 10.30         " << wm << " " << wm << endl;
+    cout << wm << " " << wm << "  Pertemuan : 6                    " << wm << " " << wm << endl;
     for (int i = 0; i < 2; i++){
-        cout << setw(80);
         for(int j = 0; j < 21; j++){
             cout << wm << " ";
         }
@@ -51,11 +48,19 @@ void count::watermark(){
 
 //fungsi untuk menginputkan nilai fpb
 void count::inputValue(){
-    cout << setw(93) << "Menghitung FPB" << endl;
-    cout << setw(106) << "Masukan Bilangan Pertama : ";
-    cin >> a;
-    cout << setw(106) << "Masukan Bilangan Kedua   : ";
-    cin >> b;
+    cout << "Menghitung FPB" << endl;
+    cout << "Masukan Bilangan Pertama : ";
+    while(!(cin >> a)){
+        cout << "Input yang anda masukkan salah! Mohon masukkan bilangan." << endl;
+        cin.clear();
+        cin.ignore();
+    }
+    cout << "Masukan Bilangan Kedua   : ";
+    while(!(cin >> b)){
+        cout << "Input yang anda masukkan salah! Mohon masukkan bilangan." << endl;
+        cin.clear();
+        cin.ignore();
+    }
     cout << endl;
 }
 
@@ -71,16 +76,14 @@ int count::process(int a, int b){
 
 //fungsi untuk mengoutputkan nilai fpb
 void count::outputValue(){
-    cout << setw(84);
     for(int i = 0; i < 17; i++){
         cout << wm << " ";
     }
     cout << endl;
-    cout << setw(84) << wm << " Hasil" << setw(26) << wm << endl;
-    cout << setw(84) << wm << " Bilangan Pertama" << setw(11) << " : " << a << "  " << wm << endl;
-    cout << setw(84) << wm << " Bilangan Kedua" << setw(13) << " : " << b << "  " << wm << endl;
-    cout << setw(84) << wm << " FPB" << setw(24) << " : " << fpb.process(a,b) << "  " << wm << endl;
-    cout << setw(84);
+    cout << wm << " Hasil" << setw(26) << wm << endl;
+    cout << wm << " Bilangan Pertama" << setw(11) << " : " << a << "  " << wm << endl;
+    cout << wm << " Bilangan Kedua" << setw(13) << " : " << b << "  " << wm << endl;
+    cout << wm << " FPB" << setw(24) << " : " << fpb.process(a,b) << "  " << wm << endl;
     for(int i = 0; i < 17; i++){
         cout << wm << " ";
     }
@@ -90,13 +93,11 @@ void count::outputValue(){
 //fungsi untuk menampilkan teks keluar dari program
 void count::exit(){
     cout << endl;
-    cout << setw(72);
     for(int i = 0; i < 29; i++){
         cout << wm << " ";
     }
     cout << endl;
-    cout << setw(72) << wm << " Terima kasih telah menggunakan program kalkulator FPB " << wm << endl;
-    cout << setw(72);
+    cout << wm << " Terima kasih telah menggunakan program kalkulator FPB " << wm << endl;
     for(int i = 0; i < 29; i++){
         cout << wm << " ";
     }
@@ -109,7 +110,7 @@ main(){
         fpb.watermark();
         fpb.inputValue();
         fpb.outputValue();
-        cout << setw(117) << "Apakah ingin mengulang program (y/n) : ";
+        cout << "Apakah ingin mengulang program (y/n) : ";
         cin >> repeat;
         if (repeat == 'y' || repeat == 'Y'){
             system("cls");
